7254740c36044fc71b0d610a6135fadf00903d60
NattapolDedruktip <dedruktip.n@gmail.com>
2024-12-03 19:20:07 +0700

    login

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,4 +1,75 @@
+import axios from "axios";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import useEcomStore from "../../store/ecom-store";
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
-  return <div>Login</div>;
+  const actionLogin = useEcomStore((state) => state.actionLogin);
+  const user = useEcomStore((state) => state.user);
+  // console.log("user from zustand ", user);
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleOnChange = (e) => {
+    console.log(e.target.name, e.target.value);
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log(form);
+    try {
+      const res = await actionLogin(form);
+      // console.log("res", res);
+      const role = res.data.payload.role;
+      console.log("role", role);
+      roleRedirect(role);
+      toast.success("Welcome Back");
+    } catch (err) {
+      // console.log(err);
+      const errMsg = err.response?.data?.message;
+      toast.error(errMsg);
+    }
+  };
+
+  const roleRedirect = (role) => {
+    if (role == "admin") {
+      navigate("/admin");
+    } else {
+      navigate("/user");
+    }
+  };
+
+  return (
+    <div>
+      Login
+      <form onSubmit={handleSubmit}>
+        Email
+        <input
+          className="border"
+          name="email"
+          type="email"
+          onChange={handleOnChange}
+        />
+        Password
+        <input
+          className="border"
+          type="password"
+          name="password"
+          onChange={handleOnChange}
+        />
+        <button className="bg-blue-300"> Login </button>
+      </form>
+    </div>
+  );
 };
 export default Login;
--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,4 +1,68 @@
+import axios from "axios";
+import { useState } from "react";
+import { toast } from "react-toastify";
+
 const Register = () => {
-  return <div>Register</div>;
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleOnChange = (e) => {
+    console.log(e.target.name, e.target.value);
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (form.password !== form.confirmPassword) {
+      return alert("confirm password is not match");
+    }
+    console.log(form);
+
+    try {
+      const res = await axios.post("http://localhost:8000/api/register", form);
+      console.log(res);
+      toast.success(res.data);
+    } catch (err) {
+      const errMsg = err.response?.data?.message;
+      toast.error(errMsg);
+      console.log(errMsg);
+    }
+  };
+
+  return (
+    <div>
+      Register
+      <form onSubmit={handleSubmit}>
+        Email
+        <input
+          className="border"
+          name="email"
+          type="email"
+          onChange={handleOnChange}
+        />
+        Password
+        <input
+          className="border"
+          type="password"
+          name="password"
+          onChange={handleOnChange}
+        />
+        Confirm Password
+        <input
+          className="border"
+          type="password"
+          name="confirmPassword"
+          onChange={handleOnChange}
+        />
+        <button className="bg-blue-300"> register </button>
+      </form>
+    </div>
+  );
 };
 export default Register;
