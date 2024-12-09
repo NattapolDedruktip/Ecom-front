d746adc8f862bf338adeb213890360a43a572f1e
NattapolDedruktip <dedruktip.n@gmail.com>
2024-12-10 00:47:21 +0700

    register page and validate with z

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,31 +1,63 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import zxcvbn from "zxcvbn";
+import { div, p, span } from "motion/react-client";
+
+const registerSchema = z
+  .object({
+    email: z.string().email({ message: "Invalid Email!!" }),
+    password: z
+      .string()
+      .min(8, { message: "password must have at least 8 characters" }),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "password is not match with ConfirmPassword",
+    path: ["confirmPassword"],
+  });
 
 const Register = () => {
+  const [passwordScore, setPasswordScore] = useState(0);
   const [form, setForm] = useState({
     email: "",
     password: "",
     confirmPassword: "",
   });
 
-  const handleOnChange = (e) => {
-    console.log(e.target.name, e.target.value);
-    setForm({
-      ...form,
-      [e.target.name]: e.target.value,
-    });
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(registerSchema),
+  });
+
+  useEffect(() => {
+    setPasswordScore(validatePassword());
+  }, [watch().password]);
+
+  console.log("passwordScore", passwordScore);
+
+  const validatePassword = () => {
+    let password = watch().password;
+    return zxcvbn(password ? password : "").score;
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (form.password !== form.confirmPassword) {
-      return alert("confirm password is not match");
+  const onSubmit = async (data) => {
+    const passwordScore = zxcvbn(data.password).score;
+    console.log(data);
+    if (passwordScore < 3) {
+      toast.warning("Password is not strong!");
+      return;
     }
-    console.log(form);
-
+    console.log("ok ready to register");
     try {
-      const res = await axios.post("http://localhost:8000/api/register", form);
+      const res = await axios.post("http://localhost:8000/api/register", data);
       console.log(res);
       toast.success(res.data);
     } catch (err) {
@@ -36,32 +68,78 @@ const Register = () => {
   };
 
   return (
-    <div>
-      Register
-      <form onSubmit={handleSubmit}>
-        Email
-        <input
-          className="border"
-          name="email"
-          type="email"
-          onChange={handleOnChange}
-        />
-        Password
-        <input
-          className="border"
-          type="password"
-          name="password"
-          onChange={handleOnChange}
-        />
-        Confirm Password
-        <input
-          className="border"
-          type="password"
-          name="confirmPassword"
-          onChange={handleOnChange}
-        />
-        <button className="bg-blue-300"> register </button>
-      </form>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="w-full shadow-md bg-white p-8 max-w-md">
+        <h1 className="text-2xl text-center my-4 font-bold">Register</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="space-y-4">
+            <div>
+              <input
+                className={`border w-full px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent ${
+                  errors.email && "border-red-500"
+                }`}
+                {...register("email")}
+                placeholder="Email..."
+              />
+              {errors.email && (
+                <p className="text-red-500 text-sm">{errors.email.message}</p>
+              )}
+            </div>
+            <div>
+              <input
+                type="password"
+                placeholder="Password ..."
+                className={`border w-full px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent ${
+                  errors.password && "border-red-500"
+                }`}
+                {...register("password")}
+              />
+              {errors.password && (
+                <p className="text-red-500 text-sm">
+                  {errors.password.message}
+                </p>
+              )}
+              {watch().password?.length > 0 && (
+                <div className="flex mt-2">
+                  {Array.from(Array(5).keys()).map((item, index) => (
+                    <span className="w-1/5 px-1">
+                      <div
+                        className={`rounded h-2 ${
+                          passwordScore <= 2
+                            ? "bg-red-500"
+                            : passwordScore < 4
+                            ? "bg-yellow-500"
+                            : "bg-green-500"
+                        }`}
+                      ></div>
+                    </span>
+                  ))}
+                </div>
+              )}
+            </div>
+
+            <div>
+              <input
+                type="password"
+                placeholder="Confirm Password ..."
+                className={`border w-full px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent ${
+                  errors.confirmPassword && "border-red-500"
+                }`}
+                {...register("confirmPassword")}
+              />
+              {errors.confirmPassword && (
+                <p className="text-red-500 text-sm">
+                  {errors.confirmPassword.message}
+                </p>
+              )}
+            </div>
+            <button className="bg-blue-500 rounded-md p-2 w-full text-white shadow-md hover:bg-blue-700">
+              {" "}
+              register{" "}
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
